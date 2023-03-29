143147a70a3e61f1dd19f0167a4e1e2047f30abd
Claudio Gonzalez <claudioggonzalez3@gmail.com>
2023-03-29 17:45:53 -0300

    actualizacion ejercicio 2

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -2,29 +2,75 @@
 #*-------------------------------------------------------------------------*
 #* factorial.py                                                            *
 #* calcula el factorial de un número                                       *
-#* Dr.P.E.Colla (c) 2022                                                   *
+#* Gonzalez Claudio                                                        *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
+#import sys
+#def factorial(num): 
+#    if num < 0: 
+#        print("Factorial de un número negativo no existe")
+
+    #elif num == 0: 
+    #    return 1
+        
+#    else: 
+#        fact = 1
+#        while(num > 1): 
+#            fact *= num 
+#            num -= 1
+#        return fact 
+
+#if len(sys.argv) == 0:
+#   print("Debe informar un número!")
+#   sys.exit()
+#num=int(sys.argv[1])
+#print("Factorial ",num,"! es ", factorial(num)) 
+
+
+#*-------------------------------------------------------------------------*
+#* factorial.py                                                            *
+#* calcula el factorial de un rango de numeros                             *
+#* Gonzalez Claudio                                                        *
+#* Creative commons                                                        *
+#*-------------------------------------------------------------------------*
+
+
 import sys
+
+num1=int(input(f"ingrese el minimo del rango"))
+num2=int(input(f"ingrese el maximo del rango"))
+
 def factorial(num): 
-    if num < 0: 
-        print("Factorial de un número negativo no existe")
+    if (num < 0 and num1 < 0): 
+        print("Factorial de un número negativo no existe o esta fuera de rango")
 
     elif num == 0: 
         return 1
         
     else: 
-        fact = 1
-        while(num > 1): 
-            fact *= num 
-            num -= 1
-        return fact 
+        lista=[]
+        for i in range(num1, num2+1):
+            lista.append(i)
+            
+            
+            for num in lista:
+                fact = 1
+                
+                while(num > 1): 
+                    fact *= num 
+                    num -= 1
+                    
+            print ("Factorial de" , i ," ,", fact)
+
 
 if len(sys.argv) == 0:
    print("Debe informar un número!")
    sys.exit()
 num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+factorial(num) 
+ 
+
+
 
 
 
